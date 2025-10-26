7ad9f815a331f7309f75c7b6a3e1bbf84a25713d
AI_Devops <harithaevanjalin@gmail.com>
2025-10-26 17:10:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,8 +48,12 @@ def index():
 
 import os
 
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     # Use 0.0.0.0 to listen on all public IPs
     # Use the PORT environment variable provided by Railway (or default to 5000)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)'''
+
+if __name__ == '__main__':
+    # ...
+    app.run(host='0.0.0.0', port=8080, debug=True)
